46a83db2fe4757082c2e261910d46733c953d76a
dm00474408 <dm00474408@INPUHJPC09773.ATT.TechMahindra.com>
2017-04-13 14:14:38 +0530

    GIT 1

---
+++ Sample 1/src/sample1_package/Sample1_class.java
@@ -0,0 +1,14 @@
+package sample1_package;
+
+public class Sample1_class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIT!");
+		System.out.println("Hello GIT1!");
+	}
+
+}
